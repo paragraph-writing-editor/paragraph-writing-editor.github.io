85ed22cd659ae973be79f3f7637508f828d9b1e5
fukuchiharuki <fukuchi.haruki@gmail.com>
2022-12-11 11:19:47 +0900

    feat: latex detection settings

--- components/SettingDialog.vue
+++ components/SettingDialog.vue
@@ -7,45 +7,61 @@ const emit = defineEmits<{
   (e: 'update:dialog', value: boolean): void
 }>()
 
-const { settingsSelection } = useSentenceBoundaryDetectionSettings()
+const { settingsSelection: sentenceBoundaryDetection } = useSentenceBoundaryDetectionSettings()
+const { settingsSelection: lineDetection } = useLineDetectionSettings()
 </script>
 
 <template>
   <ModalWindow :dialog="props.dialog" @update:dialog="(v: boolean) => emit('update:dialog', v)">
     <section>
-      <h1>Settings</h1>
+      <h1>Configuration</h1>
       <section>
         <h2>Sentence Boundary Detection</h2>
         <p>
           <label>
-            <input type="checkbox" value="halfwidthDotSpace" v-model="settingsSelection" />
+            <input type="checkbox" value="halfwidthDotSpace" v-model="sentenceBoundaryDetection" />
             <span class="code">.&nbsp;</span> <span class="note">(half-width dot followed a space)</span>
           </label>
         </p>
         <p>
           <label>
-            <input type="checkbox" value="halfwidthDotDoubleQuotationSpace" v-model="settingsSelection" />
+            <input type="checkbox" value="halfwidthDotDoubleQuotationSpace" v-model="sentenceBoundaryDetection" />
             <span class="code">."&nbsp;</span> <span class="note">(half-width dot followed a double quotation and a
               space)</span>
           </label>
         </p>
         <p>
           <label>
-            <input type="checkbox" value="halfwidthDotSingleQuotationSpace" v-model="settingsSelection" />
+            <input type="checkbox" value="halfwidthDotSingleQuotationSpace" v-model="sentenceBoundaryDetection" />
             <span class="code">.'&nbsp;</span> <span class="note">(half-width dot followed a single quotation and a
               space)</span>
           </label>
         </p>
         <p>
           <label>
-            <input type="checkbox" value="fullwidthDot" v-model="settingsSelection" />
+            <input type="checkbox" value="fullwidthDot" v-model="sentenceBoundaryDetection" />
             <span class="code">&#xff0e</span> <span class="note">(full-width dot)</span>
           </label>
         </p>
         <p>
           <label>
-            <input type="checkbox" value="fullwidthSmallCircle" v-model="settingsSelection" />
+            <input type="checkbox" value="fullwidthSmallCircle" v-model="sentenceBoundaryDetection" />
             <span class="code">。</span> <span class="note">(full-width small circle)</span>
+          </label>
+        </p>
+      </section>
+      <section>
+        <h2>Line Detection</h2>
+        <p>
+          <label>
+            <input type="checkbox" value="latexStyle" v-model="lineDetection" />
+            LaTeX/TeX style <span class="note">(such as <span class="code">\section{<i>headline</i>}</span>)</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input type="checkbox" value="markdonwStyle" v-model="lineDetection" />
+            Markdown style <span class="note">(such as <span class="code"># <i>headline</i></span>)</span>
           </label>
         </p>
       </section>
